9125590090b82e52c69de3387fe88eb3a5183a77
Agilexus <130056141+Agilexus@users.noreply.github.com>
2023-10-26 15:15:12 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 let chooseBtn = document.getElementById('choose'),
     receiveBtn = document.getElementById('receive'),
-    heading = document.getElementsByTagName('h2')[0];
+    heading = document.getElementsByTagName('h2')[0],
+    nameInput = document.getElementsByClassName('contactform_name')[0], //віддає масив, тому в кінці проставлений індекс
+    phoneInput = document.querySelector('.contactform_phone'), //бере лише перший елемент зі сторінки 
+    mailInput = document.querySelectorAll('.contactform_mail')[0]; // віддає масив
+
+function hover() {
+  heading.textContent = "Все ок";
+}
