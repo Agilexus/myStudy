54aba3e22436f36a430781de6b0a0f5fd250a0d9
Agilexus <130056141+Agilexus@users.noreply.github.com>
2023-10-26 15:41:16 +0300

    modal on

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ let chooseBtn = document.getElementById('choose'),
     heading = document.getElementsByTagName('h2')[0],
     nameInput = document.getElementsByClassName('contactform_name')[0], //віддає масив, тому в кінці проставлений індекс
     phoneInput = document.querySelector('.contactform_phone'), //бере лише перший елемент зі сторінки 
-    mailInput = document.querySelectorAll('.contactform_mail')[0]; // віддає масив
+    mailInput = document.querySelectorAll('.contactform_mail')[0], // віддає масив
+    modal = document.querySelector('.modal');
 
 function hover(text) {
   heading.textContent = 'все ок';
@@ -18,7 +19,10 @@ function hover(text) {
 // нижче найбліш правильний і популярний зараз спосіб використання івентів
 chooseBtn.addEventListener('mouseenter', hover); // те саме що вище, але якщо у нас вже є функція, то використовуємо її без ().
 
-chooseBtn.addEventListener('click', function() {
+chooseBtn.addEventListener('mouseleave', function() {
   heading.textContent = 'Нова зміна';
 });
 
+receiveBtn.addEventListener('click', function() {
+  modal.style.display = 'block';
+})
